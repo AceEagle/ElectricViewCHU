a091bffc9141d7f9d86369e9fd3c6860b9ced030
Mathieu <mathieu.vigneault.19@gmail.com>
2021-06-12 22:19:52 -0400

    Initiating Main from opt-id (PyMarc2)

--- OscilloscopeLink.py
+++ OscilloscopeLink.py
@@ -1,28 +1,5 @@
-from struct import unpack
-import pyvisa as visa
-import numpy as np
-rm = visa.ResourceManager('@py')
-print(rm)
-
-def acquire(channel, port):
-    try:
-        scope = rm.open_resource(port)
-        scope.write("DATA:SOURCE " + channel)
-        scope.write('DATA:WIDTH 1')
-        scope.write('DATA:ENC RPB')
-        ymult = float(scope.ask('WFMPRE:YMULT?'))
-        yzero = float(scope.ask('WFMPRE:YZERO?'))
-        yoff = float(scope.ask('WFMPRE:YOFF?'))
-        xincr = float(scope.ask('WFMPRE:XINCR?'))
-        xdelay = float(scope.query('HORizontal:POSition?'))
-        scope.write('CURVE?')
-        data = scope.read_raw()
-        headerlen = 2 + int(data[1])
-        header = data[:headerlen]
-        ADC_wave = data[headerlen:-1]
-        ADC_wave = np.array(unpack('%sB' % len(ADC_wave),ADC_wave))
-        Volts = (ADC_wave - yoff) * ymult  + yzero
-        Time = np.arange(0, (xincr * len(Volts)), xincr)-((xincr * len(Volts))/2-xdelay)
-        return Time,Volts
-    except IndexError:
-        return 0,0
+import pyvisa
+import time
+rm = pyvisa.ResourceManager()
+my_instrument = rm.open_resource('USB0::0x0699::0x0408::C049429::0::INSTR')
+my_instrument.write("*rst; status:preset; *cls")
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QSplashScreen
-from PyQt5.QtCore import Qt, QTimer, QSize
-from PyQt5.QtGui import QPixmap, QPainter, QMovie, QIcon, QFontDatabase
-from pyqtgraph import GraphicsLayoutWidget
-from gui.windows.mainWindow import MainWindow
-from mainModel import MainModel
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
 import sys
 import ctypes
 import logging
@@ -11,6 +8,75 @@ import logging.config
 from logging.handlers import RotatingFileHandler
 import os
 
+log = logging.getLogger(__name__)
 
 
+if getattr(sys, 'frozen', False):
+    application_path = sys._MEIPASS
+else:
+    application_path = os.path.dirname(os.path.abspath(__file__))
 
+
+class App(QApplication):
+    def __init__(self, sys_artgv):
+        super(App, self).__init__(sys_artgv)
+
+        sys.excepthook = self.handle_exception
+        self.init_logging()
+        log.debug("This is the MAIN THREAD")
+
+        self.setAttribute(Qt.AA_EnableHighDpiScaling)
+        self.setStyle("Fusion")
+
+        self.mainModel = MainModel()
+        self.mainWindow = MainWindow(model=self.mainModel)
+        self.mainWindow.setWindowTitle("PGDLive")
+        self.mainWindow.show()
+
+    @staticmethod
+    def init_logging():
+        logger = logging.getLogger()
+        logger.setLevel(logging.NOTSET)
+
+        # Console Handler
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.INFO)
+        formatter = logging.Formatter(
+            "%(asctime)s\t (%(name)-25.25s) (thread:%(thread)d) (line:%(lineno)5d)\t[%(levelname)-5.5s] %(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+        # Log Error File Handler
+        os.makedirs(application_path + "{0}log".format(os.sep), exist_ok=True)
+        handler = RotatingFileHandler(application_path + "{0}log{0}opt-id.log".format(os.sep),
+                                      maxBytes=2.3 * 1024 * 1024, backupCount=5)
+        handler.setLevel(logging.ERROR)
+        formatter = logging.Formatter(
+            "%(asctime)s\t (%(name)-30.30s) (thread:%(thread)d) (line:%(lineno)5d)\t[%(levelname)-5.5s] %(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    @staticmethod
+    def handle_exception(exc_type, exc_value, exc_traceback):
+        if issubclass(exc_type, KeyboardInterrupt):
+            sys.__excepthook__(exc_type, exc_value, exc_traceback)
+            return
+
+        log.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+
+def main():
+    # Makes the icon in the taskbar as well.
+    appID = "PGDLive"  # arbitrary string
+    if os.name == 'nt':
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(appID)
+    elif os.name == 'posix':
+        pass
+    app = App(sys.argv)
+    app.setWindowIcon(QIcon(application_path + "{0}gui{0}misc{0}logo{0}logo3.ico".format(os.sep)))
+    app.mainWindow.setWindowIcon(QIcon(application_path + "{0}gui{0}misc{0}logo{0}logo3.ico".format(os.sep)))
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
