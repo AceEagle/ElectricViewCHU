828b84d2a2a2cdbc97136ed4bd064bb7f70528bb
Mathieu <mathieu.vigneault.19@gmail.com>
2021-06-21 12:54:30 -0400

    Poslished the dataview and trying again to communicate by pyvisa

--- OscilloscopeLink.py
+++ OscilloscopeLink.py
@@ -1,8 +1,6 @@
 import pyvisa
 
-number_of_readings = 10
-interval_in_ms = 500
+
 rm = pyvisa.ResourceManager()
 print(rm.list_resources())
-my_instrument = rm.open_resource('USB0::0x0699::0x0408::C049429::0::INSTR')
-my_instrument.('*rst; status:preset; *cls')
+
