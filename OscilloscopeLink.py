86f0ddf5cd28233efb19e8b17aea95d983a728a9
Mathieu <mathieu.vigneault.19@gmail.com>
2021-06-22 16:28:12 -0400

    Worked to setup parameters on AFG AND Oscillo, but getting live data seems to be more complicated.

--- OscilloscopeLink.py
+++ OscilloscopeLink.py
@@ -1,13 +1,19 @@
 import pyvisa
-
+import time
 
 rm = pyvisa.ResourceManager()
 print(rm.list_resources())
-my_instr = rm.open_resource('USB0::0x0699::0x03A3::C020282::0::INSTR')
-print(my_instr.query("*IDN?"))
-print(my_instr.query("HOR:SCA?"))
-print(my_instr.query("MEASU?"))
+my_instr = rm.open_resource('USB0::0x0699::0x0408::C049429::0::INSTR')
+#print(my_instr.query("*IDN?"))
+#print(my_instr.query("HOR:SCA?"))
+#print(my_instr.query("MEASU?"))
 #my_instr.query("SAV:SET?")
-print(my_instr.query("TRIG:A:LEV?"))
-print(my_instr.query("CH1:PRO?"))
-print(my_instr.query("CH1:SCA?"))
+#print(my_instr.query("TRIG:A:LEV?"))
+#print(my_instr.query("CH1:PRO?"))
+#print(my_instr.query("CH1:SCA?"))
+
+#-------------------------------------------------------------
+
+my_instr.write("SINGle")
+print(my_instr.query("*OPC?"))
+print(my_instr.query("CHAN:DATA？"))
