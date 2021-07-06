b2e651627676a2a6804c50f709a9177c5180482d
Mathieu <mathieu.vigneault.19@gmail.com>
2021-07-06 14:58:58 -0400

    Deleting obsolete PlasmaAnalyzer functions

--- PlasmaAnalyser.py
+++ PlasmaAnalyser.py
@@ -19,16 +19,8 @@ class PlasmaAnalyser(QObject):
 
     def __init__(self):
         super(PlasmaAnalyser, self).__init__()
-        self.timeNow = 0
-        self.timePast = 0
-        self.day = 0
-        self.selectedgraphics = []
-        self.population = []
-        self.parameters = {}
         self.savedStatusDataDict = {}
         self.create_empty_savedStatusDataDict()
-        self.data2plot = {}
-        self.healthcareSystemLimit = 50000
         self.connect_to_signals()
         self.myOscillo = None
         self.myAFG = None
@@ -62,11 +54,6 @@ class PlasmaAnalyser(QObject):
         self.initialize_infection(nbOfInfected=args[1])
         self.launch_propagation(args[2])
 
-    def simulate(self, amount, time, parameters=None):
-        self.create_population(amount, parameters)
-        self.initialize_infection(nbOfInfected=1)
-        self.launch_propagation(time)
-
     def plot_results(self, graphic):
         fig, ax1 = plt.subplots(figsize=(4, 4))
         xdata = range(len(self.savedStatusDataDict))
@@ -95,46 +82,11 @@ class PlasmaAnalyser(QObject):
         self.launch_state = False
 
     def reset_save_status(self):
-        for graphic in Data().graphics:
-            self.savedStatusDataDict[graphic]["data"]["x"] = []
-            self.savedStatusDataDict[graphic]["data"]["y"] = []
+        self.create_empty_savedStatusDataDict()
 
     def save_status(self):
         for graphic in Data().graphics:
             self.savedStatusDataDict[graphic]["data"]["x"].append(self.day)
             self.savedStatusDataDict[graphic]["data"]["y"].append (
                 sum(p.graphics[graphic] == 1 and p.tag == ageKey for p in self.population))
-
-    def initialize_infection(self, nbOfInfected=1):
-        try:
-            indexes = random.choices(range(len(self.population)), k=nbOfInfected)
-            for index in indexes:
-                self.population[index].graphics["isInfected"] = 1
-        except Exception as e:
-            log.error(e)
-
-    def create_population(self, amountOfPeople, parameters=None):
-        if parameters is not None:
-            self.parameters = parameters
-
-        id = 0
-        for i, key in enumerate(self.parameters.keys()):
-            for j in range(int(amountOfPeople * self.parameters[key]["percentageOfPopulation"])):
-                randomizedParameters = self.give_gaussian_parameters(self.parameters[key])
-                self.population.append(Data(randomizedParameters, tag=key, id=id))
-                id += 1
-            log.info("Creating Population...{}/{}".format(i, len(self.parameters.keys())))
-        random.shuffle(self.population)
-        log.info("Done.")
-        log.info("Randomizing Relationships...")
-        for person in self.population:
-            person.select_relatives(self.population, 50)
-        log.info("Done.")
-        return self.population
-
-    def load_json_parameters(self, jsonFilePath):
-        with open(jsonFilePath) as f:
-            parametersFile = json.load(f)
-        self.parameters = parametersFile[0]
-        return self.parameters
 
