5c229783ca7714741e35860fd379ecd5b6e313d7
Mathieu <mathieu.vigneault.19@gmail.com>
2021-07-08 16:00:19 -0400

    Just a quicksave (tried some things in scratch to transform binary to ascii (cant directly get ascii from oscilloscope)

--- OscilloscopeLinkScratch2.py
+++ OscilloscopeLinkScratch2.py
@@ -37,28 +37,28 @@ for i in range(0, len(values) - 1):
 f.close()
 print("Waveform format BYTE data written to waveform_data.csv.")
 """
-
-x_increment = instrumentObject.query_ascii_values(":WAVeform:XINCrement?")
-x_origin =  instrumentObject.query_ascii_values(":WAVeform:XORigin?")
-y_increment =  instrumentObject.query_ascii_values(":WAVeform:YINCrement?")
-y_origin =  instrumentObject.query_ascii_values(":WAVeform:YORigin?")
-y_reference =  instrumentObject.query_ascii_values(":WAVeform:YREFerence?")
+#instrumentObject.write('DATa:RESOlution FULL, DATa:COMPosition SINGULAR_YT, DATa:DESTination 5000, DATa:STARt 1, DATa:ENCdg ASCIi')
+a = instrumentObject.query_binary_values("WAVFrm?")
+print(a)
+#x_origin =  instrumentObject.query_ascii_values(":WAVeform:XORigin?")
+#y_increment =  instrumentObject.query_ascii_values(":WAVeform:YINCrement?")
+#y_origin =  instrumentObject.query_ascii_values(":WAVeform:YORigin?")
+#y_reference =  instrumentObject.query_ascii_values(":WAVeform:YREFerence?")
 # Get the waveform data.
-instrumentObject.write(":WAVeform:DATA?")
-sData = instrumentObject.read_bytes(50000)
-print(sData)
+#instrumentObject.write(":WAVeform:DATA?")
+#sData = instrumentObject.read_bytes(50000)
 # sData = get_definite_length_block_data(sData)
 # Unpack unsigned byte data.
-values = struct.unpack("%dB" % len(sData), sData)
-print("Number of data values: %d" % len(values))
+#values = struct.unpack("%dB" % len(sData), sData)
+#print("Number of data values: %d" % len(values))
 # Save waveform data values to CSV file.
-f = open("waveform_data.csv", "w")
-for i in range(0, len(values) - 1):
-    time_val = x_origin[0] + (i * x_increment[0])
-    #print(values[i], y_reference, y_increment, y_origin)
-    voltage = ((values[i] - y_reference[0]) * y_increment[0]) + y_origin[0]
-    f.write("%E, %f\n" % (time_val, voltage))
-f.close()
-print("Waveform format BYTE data written to waveform_data.csv.")
+#f = open("waveform_data.csv", "w")
+#for i in range(0, len(values) - 1):
+#    time_val = x_origin[0] + (i * x_increment[0])
+#    #print(values[i], y_reference, y_increment, y_origin)
+#    voltage = ((values[i] - y_reference[0]) * y_increment[0]) + y_origin[0]
+#    f.write("%E, %f\n" % (time_val, voltage))
+#f.close()
+#print("Waveform format BYTE data written to waveform_data.csv.")
 
 
