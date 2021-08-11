0d18ccbc43d9b4f1d5d2718ccf3d58e3b19ac362
Mathieu <mathieu.vigneault.19@gmail.com>
2021-08-10 20:40:37 -0400

    tests in equaiton test

--- EquationsTest.py
+++ EquationsTest.py
@@ -1,19 +1,17 @@
 import math
-
+import numpy as np
+from matplotlib import pyplot as plt
 from scipy import integrate
 sinus = []
 cosin = []
+xList = []
 multiplied = []
 finished = []
-for x in range(50):
+for x in range(10000):
     sinus.append(math.sin(x))
     cosin.append(math.cos(x))
-    multiplied.append(sinus[x]*cosin[x])
-integrated = integrate.cumtrapz(multiplied)
-finished.append(5000*integrated/20)
+    xList.append(x)
 
-print(sinus)
-print(cosin)
-print(multiplied)
-print(integrated)
-print(finished)
+multiplied = np.multiply(sinus, cosin)
+ptlist = 10000 * integrate.trapezoid(multiplied) / (20 * 50)
+print(ptlist)
