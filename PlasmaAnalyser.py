f7003671558496cb7dd0d29a9d4d02967ed1d17f
Mathieu <mathieu.vigneault.19@gmail.com>
2021-08-11 10:12:53 -0400

    Revert "cycles percentage to cycles number"
    This reverts commit 5d12ce5ba432fe1136757f6380e073326cb047c6.

--- PlasmaAnalyser.py
+++ PlasmaAnalyser.py
@@ -17,8 +17,6 @@ log = logging.getLogger(__name__)
 
 SIGNAL_PLOT_TOGGLED = "plot.toggled.graphic"
 
-
-# noinspection PyUnresolvedReferences
 class PlasmaAnalyser(QObject):
     #s_data_changed = pyqtSignal(dict, list, int)
     s_data_changed = pyqtSignal(dict)
@@ -36,7 +34,6 @@ class PlasmaAnalyser(QObject):
         self.instrumentsDict = {"myOscillo": None, "myAFG": None}
         self.xList = []
         self.surface = 0
-        self.trigInterval = 0
         for x in range(100000):
             self.xList.append(x)
         self.timeList = []
@@ -60,7 +57,7 @@ class PlasmaAnalyser(QObject):
         self.instrumentsDict["myAFG"].write(f"SOURce1:{mode}:MODE")
         self.instrumentsDict["myAFG"].write(f":SOURCE:FREQUENCY {freq}KHZ")
         self.instrumentsDict["myAFG"].write(f"SOURce1:FUNCtion:{wave}")
-        if cycle != 0:
+        if cycle is not 0:
             self.instrumentsDict["myAFG"].write(f"SOURce1:BURSt:NCYCles {cycle}")
 
     def inject_Oscillo(self, nbData):
